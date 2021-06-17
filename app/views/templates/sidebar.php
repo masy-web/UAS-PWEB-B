0d246cee155dfedb502db3f0c5cb8f8c4638adec
masy-web <72302847+masy-web@users.noreply.github.com>
2021-06-17 20:31:53 +0700

    Update sidebar.php

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -1,8 +1,8 @@
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <span class="brand-text font-weight-light">Buku Kita</span>
+      <span class="brand-text font-weight-light">Satwa Ku</span>
     </a>
 
     <!-- Sidebar -->
@@ -10,7 +10,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="info">
-          <a href="#" class="d-block">@herziwp</a>
+          <a href="#" class="d-block">@uaspwebB</a>
         </div>
       </div>
 
