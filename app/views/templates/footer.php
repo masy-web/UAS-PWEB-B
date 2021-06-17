4498e4e8b01cb0217c5ee1e41034c924d11e87a8
masy-web <72302847+masy-web@users.noreply.github.com>
2021-06-17 20:27:16 +0700

    Update footer.php

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,7 @@
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 1.0
     </div>
     <strong>Copyright &copy;2021 SatwaKu All Right 
     reserved.
